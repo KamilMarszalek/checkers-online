9354e44ac349a99d3971c5dc12d4094168f5795a
KamilMarszalek <kamilmar553@gmail.com>
2025-01-24 22:05:21 +0100

    Added GameState class

---
+++ src/main/java/pw/checkers/pojo/GameState.java
@@ -0,0 +1,19 @@
+package pw.checkers.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GameState {
+    private String gameId;
+    private Piece[][] board;
+    private String currentPlayer;
+    private boolean isFinished;
+    private String winner;
+}
