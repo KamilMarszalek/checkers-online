23df80a6aaac36d614053ff1780f9437834eb106
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-26 13:41:13 +0100

    Changed STOMP websocket to standard websocket

--- src/main/java/pw/checkers/config/WebSocketConfig.java
+++ src/main/java/pw/checkers/config/WebSocketConfig.java
@@ -2,21 +2,21 @@ package pw.checkers.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
+import pw.checkers.service.GameService;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue");
-        config.setApplicationDestinationPrefixes("/app");
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final GameService gameService;
+
+    public WebSocketConfig(GameService gameService) {
+        this.gameService = gameService;
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MyWebSocketHandler(gameService), "/ws")
+                .setAllowedOrigins("*");
     }
 }
