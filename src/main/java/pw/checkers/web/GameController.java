e5a181c9ac6b7e95e3166289d22a7b0ad32236c5
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-26 13:40:18 +0100

    Deleted STOMP socket

--- src/main/java/pw/checkers/web/GameController.java
+++ src/main/java/pw/checkers/web/GameController.java
@@ -1,15 +1,11 @@
 package pw.checkers.web;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pw.checkers.pojo.GameState;
-import pw.checkers.pojo.Move;
 import pw.checkers.service.GameService;
 
 @Controller
@@ -17,12 +13,6 @@ public class GameController {
     private final GameService gameService;
     public GameController(GameService gameService) {
         this.gameService = gameService;
-    }
-
-    @MessageMapping("/game/{gameId}/move")
-    @SendTo("topic/game/{gameId}")
-    public GameState processMove(@DestinationVariable String gameId, Move move) {
-        return gameService.makeMove(gameId, move);
     }
 
     @PostMapping("/api/game")
