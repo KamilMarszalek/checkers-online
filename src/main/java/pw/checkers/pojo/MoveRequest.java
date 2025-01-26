20ab7f3eb4bbc5bdd0bc38795d6a5e961e0904da
KamilMarszalek <kamilmar553@gmail.com>
2025-01-26 13:40:36 +0100

    Created MoveRequest pojo

---
+++ src/main/java/pw/checkers/pojo/MoveRequest.java
@@ -0,0 +1,15 @@
+package pw.checkers.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MoveRequest {
+    private String gameId;
+    private Move move;
+}
