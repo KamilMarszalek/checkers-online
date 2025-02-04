aedc74f97c61f6a7d59ad1c6b9965c4eef435433
KamilMarszalek <kamilmar553@gmail.com>
2025-02-04 17:08:50 +0100

    Added possible moves pojo

---
+++ backend/src/main/java/pw/checkers/pojo/PossibleMoves.java
@@ -0,0 +1,16 @@
+package pw.checkers.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PossibleMoves {
+    private List<Move> moves;
+}
