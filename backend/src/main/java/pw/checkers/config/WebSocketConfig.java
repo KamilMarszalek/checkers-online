74afd0c5bdf2a7972ab36ed2e9cc20f0bdac5e7e
Kamil Marszałek <01187250@pw.edu.pl>
2025-02-04 22:46:26 +0100

    Renamed handler

--- backend/src/main/java/pw/checkers/config/WebSocketConfig.java
+++ backend/src/main/java/pw/checkers/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyWebSocketHandler(gameService), "/ws")
+        registry.addHandler(new CheckersWebSocketHandler(gameService), "/ws")
                 .setAllowedOrigins("*");
     }
 }
