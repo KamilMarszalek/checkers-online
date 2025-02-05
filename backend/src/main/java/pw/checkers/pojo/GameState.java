ca279249fe2627087c949b58948b44ed8aafc588
Kamil Marszałek <01187250@pw.edu.pl>
2025-02-05 21:50:27 +0100

    Added method for hashing board

--- backend/src/main/java/pw/checkers/pojo/GameState.java
+++ backend/src/main/java/pw/checkers/pojo/GameState.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Map;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -18,4 +20,21 @@ public class GameState {
     private String winner;
     private int whitePiecesLeft;
     private int blackPiecesLeft;
+    private int noCapturesCounter;
+    private Map<String, Integer> numberOfPositions;
+
+    public String boardToString() {
+        StringBuilder response = new StringBuilder();
+        for (Piece[] row : board) {
+            for (Piece piece : row) {
+                if (piece == null) {
+                    response.append(".");
+                } else {
+                    response.append(piece.toString());
+                }
+            }
+        }
+        response.append(currentPlayer);
+        return response.toString();
+    }
 }
