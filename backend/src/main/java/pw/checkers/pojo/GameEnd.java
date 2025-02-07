13fec21db242cd959357738c328617bbe94a7eac
KamilMarszalek <kamilmar553@gmail.com>
2025-02-07 21:55:43 +0100

    Added simple pojo to send message when game ends

---
+++ backend/src/main/java/pw/checkers/pojo/GameEnd.java
@@ -0,0 +1,14 @@
+package pw.checkers.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameEnd {
+    private String result;
+}
