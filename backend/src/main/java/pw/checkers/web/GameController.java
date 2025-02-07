fcb0784fcdfa54c36251e2d2ec86f83a027c125b
KamilMarszalek <kamilmar553@gmail.com>
2025-02-07 12:21:45 +0100

    Changed controller to rest controller

--- backend/src/main/java/pw/checkers/web/GameController.java
+++ backend/src/main/java/pw/checkers/web/GameController.java
@@ -1,27 +1,28 @@
 package pw.checkers.web;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import pw.checkers.pojo.GameState;
 import pw.checkers.service.GameService;
 
-@Controller
+@RestController
+@RequestMapping("/game")
+@Tag(name="GameController", description = "Controller to create and retrieve games")
 public class GameController {
     private final GameService gameService;
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
 
-    @PostMapping("/api/game")
+    @PostMapping("")
     public ResponseEntity<GameState> createGame() {
         GameState gameState = gameService.createGame();
         return ResponseEntity.ok(gameState);
     }
 
-    @GetMapping("/api/game/{gameId}")
+    @GetMapping("/{gameId}")
     public ResponseEntity<GameState> getGame(@PathVariable String gameId) {
         GameState gameState = gameService.getGame(gameId);
         if (gameState == null) {
