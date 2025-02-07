b378250a50b4c8d4af026a04f1c2c3872f291fc2
Kamil Marszałek <01187250@pw.edu.pl>
2025-02-07 12:22:18 +0100

    Deleted unused import statement

--- backend/src/main/java/pw/checkers/web/GameController.java
+++ backend/src/main/java/pw/checkers/web/GameController.java
@@ -2,7 +2,6 @@ package pw.checkers.web;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pw.checkers.pojo.GameState;
 import pw.checkers.service.GameService;
