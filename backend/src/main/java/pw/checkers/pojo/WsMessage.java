185bd6d5c9d01ed0f49c5d33a6e5c7ec0769b770
KamilMarszalek <kamilmar553@gmail.com>
2025-02-10 23:32:59 +0100

    Fixed joinQueue parsing

--- backend/src/main/java/pw/checkers/pojo/WsMessage.java
+++ backend/src/main/java/pw/checkers/pojo/WsMessage.java
@@ -1,5 +1,6 @@
 package pw.checkers.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WsMessage {
     private String type;
     private String gameId;
