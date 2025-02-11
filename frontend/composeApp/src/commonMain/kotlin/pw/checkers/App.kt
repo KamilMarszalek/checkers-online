c2ae5be578bc8415aaa08d865d08b8d0a0fbd036
damian-ds7 <dsouzadamian189@gmail.com>
2025-02-11 16:24:26 +0100

    Updated UI to change size based on window size, added user data placeholders

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/App.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/App.kt
@@ -1,14 +1,42 @@
 package pw.checkers
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import pw.checkers.models.initialBoard
 import pw.checkers.ui.Board
+import pw.checkers.ui.theme.AppTheme
+import pw.checkers.ui.windowSize.rememberWindowSize
+import pw.checkers.util.calcCellSize
 
 @Composable
 fun App() {
-    println("sad")
-    MaterialTheme {
-        Board(initialBoard, emptySet()) { x, y -> println("$x $y") }
+    val windowSize = rememberWindowSize()
+    val cellSize = remember(windowSize) { calcCellSize(windowSize.first, windowSize.second) }
+    AppTheme {
+        Scaffold {
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                Column {
+                    UserPanelPlaceHolder(cellSize * 8, height = cellSize)
+                    Board(initialBoard, emptySet(), cellSize) { x, y -> println("$x $y") }
+                    UserPanelPlaceHolder(cellSize * 8, height = cellSize)
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun UserPanelPlaceHolder(width: Dp, height: Dp) {
+    Box(
+        modifier = Modifier
+            .width(width)
+            .height(height)
+            .background(Color.Blue)
+    )
 }
