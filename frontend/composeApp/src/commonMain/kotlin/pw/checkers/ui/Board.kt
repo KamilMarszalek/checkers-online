6c9c065d59743e091452c51b4f50a1c9d4e2110d
damian-ds7 <dsouzadamian189@gmail.com>
2025-02-11 16:22:14 +0100

    Added cellSize argument

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/ui/Board.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/ui/Board.kt
@@ -3,12 +3,12 @@ package pw.checkers.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Dp
 import pw.checkers.models.*
 import pw.checkers.ui.icons.Pawns
 import pw.checkers.ui.icons.pawns.BlackPawn
@@ -19,14 +19,15 @@ import pw.checkers.ui.icons.pawns.WhiteQueen
 fun Board(
     board: Board,
     highlightedCells: Set<Pair<Int, Int>>,
-    onCellClick: (row: Int, col: Int) -> Unit
+    cellSize: Dp,
+    onCellClick: (row: Int, col: Int) -> Unit,
 ) {
     Column {
         board.forEachIndexed { rowIndex, row ->
             Row {
                 row.forEachIndexed { colIndex, cell ->
                     val isHighlighted = highlightedCells.contains(rowIndex to colIndex)
-                    Cell(cell, isHighlighted, onCellClick)
+                    Cell(cell, cellSize, isHighlighted, onCellClick)
                 }
             }
         }
@@ -36,14 +37,14 @@ fun Board(
 @Composable
 private fun Cell(
     cell: CellState,
+    cellSize: Dp,
     isHighlighted: Boolean,
     onClick: (row: Int, col: Int) -> Unit
 ) {
     Box(
         modifier = Modifier
-            .size(60.dp)
+            .size(cellSize)
             .background(if ((cell.row + cell.col) % 2 == 0) Color.LightGray else Color.DarkGray),
-//            .clickable { onClick(cell.row, cell.col) },
         contentAlignment = Alignment.Center
     ) {
         cell.piece?.let { piece ->
