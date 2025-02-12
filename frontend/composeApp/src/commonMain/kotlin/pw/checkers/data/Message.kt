8d1d44e9641f25f83daa85c64a1a0ca238843904
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-13 00:24:50 +0100

    fixed message content from Any to String

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/data/Message.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/data/Message.kt
@@ -1,7 +1,9 @@
 package pw.checkers.data
 
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Message(
     val type: String,
-    val content: Any?
+    val content: String,
 )
