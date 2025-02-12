0e5041e8a9b7c85d19040241f7ef8e29b4e96704
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-13 00:24:28 +0100

    modified InitialBoard to a function with option to reverse if player is black

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/models/InitialBoard.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/models/InitialBoard.kt
@@ -2,12 +2,22 @@ package pw.checkers.models
 
 typealias Board = List<List<CellState>>
 
-val initialBoard: Board = List(8) { row ->
-    List(8) { col ->
-            when {
-                row < 3 && (row + col) % 2 == 1 -> CellState(row, col, Piece(PieceColor.BLACK, PieceType.Pawn))
-                row > 4 && (row + col) % 2 == 1 -> CellState(row, col, Piece(PieceColor.WHITE, PieceType.Pawn))
-                else -> CellState(row, col)
+fun createInitialBoard(reverse: Boolean = false): Board {
+    val (topColor, bottomColor) = if (reverse)
+        PieceColor.WHITE to PieceColor.BLACK
+    else
+        PieceColor.BLACK to PieceColor.WHITE
+
+    return List(8) { row ->
+        List(8) { col ->
+            CellState(
+                row, col, when {
+                    (row + col) % 2 == 0 -> null
+                    row < 3 -> Piece(topColor, PieceType.Pawn)
+                    row > 4 -> Piece(bottomColor, PieceType.Pawn)
+                    else -> null
+                }
+            )
         }
     }
 }
