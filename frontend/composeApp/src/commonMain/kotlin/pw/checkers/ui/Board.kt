841227544c5a0223e2ca8eddb54f26f31109ed1d
damian-ds7 <dsouzadamian189@gmail.com>
2025-02-13 22:17:20 +0100

    refactored cell to use one clickable modifier

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/ui/Board.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/ui/Board.kt
@@ -1,8 +1,10 @@
 package pw.checkers.ui
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -16,6 +18,7 @@ import androidx.compose.ui.unit.times
 import pw.checkers.models.*
 import pw.checkers.ui.icons.Pawns
 import pw.checkers.ui.icons.pawns.BlackPawn
+import pw.checkers.ui.icons.pawns.BlackQueen
 import pw.checkers.ui.icons.pawns.WhitePawn
 import pw.checkers.ui.icons.pawns.WhiteQueen
 import pw.checkers.util.PlayerColor
@@ -30,11 +33,18 @@ fun Board(
     val highlightedCells by viewModel.highlightedCells.collectAsState()
 
     Column {
-        board.forEachIndexed { rowIndex, row ->
+        board.forEach { row ->
             Row {
-                row.forEachIndexed { colIndex, cell ->
-                    val isHighlighted = highlightedCells.contains(rowIndex to colIndex)
-                    Cell(cell, cellSize, isHighlighted, viewModel::getPossibleMoves, viewModel::makeMove)
+                row.forEach { cell ->
+                    val isHighlighted = highlightedCells.contains(cell.row to cell.col)
+                    Cell(
+                        cell,
+                        cellSize,
+                        isHighlighted,
+                        viewModel::getPossibleMoves,
+                        viewModel::makeMove,
+                        viewModel::unselectPiece
+                    )
                 }
             }
         }
@@ -47,35 +57,40 @@ private fun Cell(
     cellSize: Dp,
     isHighlighted: Boolean,
     onPieceClick: (row: Int, col: Int) -> Unit,
-    onHighlightedClick: (row: Int, col: Int) -> Unit
+    onHighlightedClick: (row: Int, col: Int) -> Unit,
+    onEmptyClick: (row: Int, col: Int) -> Unit,
 ) {
+    val clickFunction = when {
+        isHighlighted -> onHighlightedClick
+        cell.piece != null -> onPieceClick
+        else -> onEmptyClick
+    }
     Box(
         modifier = Modifier.size(cellSize)
-            .background(if ((cell.row + cell.col) % 2 == 0) Color.LightGray else Color.DarkGray),
+            .background(if ((cell.row + cell.col) % 2 == 0) Color.LightGray else Color.DarkGray)
+            .clickable(
+                interactionSource = MutableInteractionSource(),
+                indication = if (clickFunction == onEmptyClick) null else LocalIndication.current
+            ) { clickFunction(cell.row, cell.col) },
         contentAlignment = Alignment.Center
     ) {
         if (isHighlighted) {
-            Box(
-                modifier = Modifier.fillMaxSize().clickable { onHighlightedClick(cell.row, cell.col) },
-                contentAlignment = Alignment.Center
-            ) {
-                Canvas(
-                    modifier = Modifier
-                        .size(0.4 * cellSize),
-                    onDraw = { drawCircle(color = Color.Red) }
-                )
-            }
+            Canvas(
+                modifier = Modifier
+                    .size(0.4 * cellSize),
+                onDraw = { drawCircle(color = Color.Red) }
+            )
         }
 
         cell.piece?.let { piece ->
             val icon = when (piece.color) {
                 PlayerColor.WHITE -> if (piece.type == PieceType.PAWN) Pawns.WhitePawn else Pawns.WhiteQueen
-                PlayerColor.BLACK -> if (piece.type == PieceType.PAWN) Pawns.BlackPawn else Pawns.BlackPawn
+                PlayerColor.BLACK -> if (piece.type == PieceType.PAWN) Pawns.BlackPawn else Pawns.BlackQueen
             }
             Icon(
                 icon,
                 contentDescription = "Checkers piece",
-                modifier = Modifier.fillMaxSize().clickable { onPieceClick(cell.row, cell.col) },
+                modifier = Modifier.fillMaxSize(),
                 tint = Color.Unspecified,
             )
         }
