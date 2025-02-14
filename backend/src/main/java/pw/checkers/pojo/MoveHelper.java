ddf8f2fbf7cd8279e32644ffc31169f398452526
KamilMarszalek <kamilmar553@gmail.com>
2025-02-14 22:45:20 +0100

    Added MoveHelper class

---
+++ backend/src/main/java/pw/checkers/pojo/MoveHelper.java
@@ -0,0 +1,16 @@
+package pw.checkers.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MoveHelper {
+    private int row;
+    private int col;
+}
