f8c3516f57b8838f3b7e05e09fdfad182129e607
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-16 00:14:01 +0100

    move message types into enum class

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/util/Constants.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/util/Constants.kt
@@ -2,10 +2,4 @@ package pw.checkers.util
 
 object Constants {
     const val SERVER_ADDRESS = "ws://localhost:8080/ws"
-    const val TYPE_GAME_CREATED = "Game created"
-    const val TYPE_MOVED = "move"
-    const val TYPE_POSSIBILITIES = "possibilities"
-    const val TYPE_WAITING = "waiting"
-    const val TYPE_GAME_ENDING = "gameEnd"
-    const val TYPE_JOIN_QUEUE = "joinQueue"
 }
