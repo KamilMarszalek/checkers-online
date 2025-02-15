bfdc8fd8401da8f2c4f3838aeb28b4941dd6b48f
KamilMarszalek <kamilmar553@gmail.com>
2025-02-15 22:39:50 +0100

    Prepared structure for handling timeUpdates

--- backend/src/main/java/pw/checkers/config/TimeUpdatesWebSocketHandler.java
+++ backend/src/main/java/pw/checkers/config/TimeUpdatesWebSocketHandler.java
@@ -1,13 +1,36 @@
 package pw.checkers.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import pw.checkers.pojo.Message;
 import pw.checkers.service.GameService;
+
+import java.util.Map;
 
 public class TimeUpdatesWebSocketHandler extends TextWebSocketHandler {
     private final GameService gameService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
     public TimeUpdatesWebSocketHandler(GameService gameService) {
         this.gameService = gameService;
     }
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("Time Updates WebSocket connection established: " + session.getId());
+    }
 
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("Time Updates WebSocket connection closed: " + session.getId());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        Message<Map<String, Object>> rawMessage = objectMapper.readValue(message.getPayload(), new TypeReference<>() {});
+
+    }
 }
