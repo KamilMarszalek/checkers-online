29da435c5e34d8cda678283b658a2edd9282b9d9
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-15 12:00:26 +0100

    Implement basic realtime message client

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/client/KtorRealtimeMessageClient.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/client/KtorRealtimeMessageClient.kt
@@ -1,4 +1,40 @@
 package pw.checkers.client
 
-class RealtimeMessageClientImpl {
+import io.ktor.client.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.client.request.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.flow.*
+import kotlinx.serialization.json.Json
+import pw.checkers.data.message.Message
+import pw.checkers.util.Constants
+
+class KtorRealtimeMessageClient(private val httpClient: HttpClient) : RealtimeMessageClient {
+
+    private var session: WebSocketSession? = null
+
+    override suspend fun getMessageStream(): Flow<Message> {
+        return flow {
+            session = httpClient.webSocketSession {
+                url(Constants.SERVER_ADDRESS)
+            }
+
+            val messages = session!!
+                .incoming
+                .consumeAsFlow()
+                .filterIsInstance<Frame.Text>()
+                .mapNotNull { Json.decodeFromString<Message>(it.readText()) }
+
+            emitAll(messages)
+        }
+    }
+
+    override suspend fun sendMessage(message: Message) {
+        session?.outgoing?.send(Frame.Text(Json.encodeToString(message)))
+    }
+
+    override suspend fun closeConnection() {
+        session?.close(CloseReason(CloseReason.Codes.NORMAL, "Close connection"))
+        session = null
+    }
 }
--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/client/RealtimeMessageClient.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/client/RealtimeMessageClient.kt
@@ -1,4 +1,10 @@
 package pw.checkers.client
 
+import kotlinx.coroutines.flow.Flow
+import pw.checkers.data.message.Message
+
 interface RealtimeMessageClient {
+    suspend fun getMessageStream(): Flow<Message>
+    suspend fun sendMessage(message: Message)
+    suspend fun closeConnection()
 }
