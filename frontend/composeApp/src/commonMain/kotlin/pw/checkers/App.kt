beac4e3d8891e08253aa382ff8f369173ebf4df7
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-15 21:05:19 +0100

    add Koin initialization, move screen composable to Checkers()

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/App.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/App.kt
@@ -1,28 +1,37 @@
 package pw.checkers
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
-import pw.checkers.data.domain.PlayerColor
-import pw.checkers.ui.Board
-import pw.checkers.ui.screens.GameScreen
+import org.koin.compose.KoinContext
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import pw.checkers.di.appModule
 import pw.checkers.ui.theme.AppTheme
-import pw.checkers.ui.windowSize.rememberWindowSize
-import pw.checkers.util.*
-import pw.checkers.viewModel.GameViewModel
 
 @Composable
 fun App() {
-    val viewModel = GameViewModel(color = PlayerColor.BLACK)
+
+    remember {
+        startKoin {
+            modules(appModule)
+        }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            stopKoin()
+        }
+    }
+
     AppTheme {
         Scaffold {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                GameScreen(viewModel)
+            KoinContext {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Checkers()
+                }
             }
         }
     }
