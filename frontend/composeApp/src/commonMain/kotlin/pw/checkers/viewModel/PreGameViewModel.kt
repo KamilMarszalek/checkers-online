709027efbf232e2ef4a1e6d3de22b86042ab3941
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-15 20:51:45 +0100

    rename PreGameViewModel to LoginViewModel, implement basic LoginViewModel

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/viewModel/PreGameViewModel.kt
+++
@@ -1,9 +0,0 @@
-package pw.checkers.viewModel
-
-import androidx.lifecycle.ViewModel
-import pw.checkers.client.RealtimeMessageClient
-import pw.checkers.data.message.Message
-import pw.checkers.util.Constants
-
-class PreGameViewModel(messageClient: RealtimeMessageClient) : ViewModel() {
-}
