7194c94d77641f09357375667de4f33a7542f21f
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-16 19:00:06 +0100

    implement type safe navigation

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/Checkers.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/Checkers.kt
@@ -1,14 +1,13 @@
 package pw.checkers
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
@@ -21,28 +20,33 @@ fun Checkers(navController: NavHostController = rememberNavController()) {
 
     NavHost(
         navController = navController,
-        startDestination = "login",
-        modifier = Modifier.fillMaxSize(),
+        startDestination = Routes.Login,
+//        modifier = Modifier.fillMaxSize(),
     ) {
-        composable("login") {
+        composable<Routes.Login> {
             LoginScreen(
                 koinViewModel(),
                 onLoginClick = { gameCreated ->
-                    val gameDataJson = Json.encodeToString(gameCreated)
-                    navController.navigate(route = "game?data=$gameDataJson")
+                    navController.navigate(Routes.Game(Json.encodeToString(gameCreated)))
                 }
             )
         }
 
-        composable(
-            route = "game?data={data}",
-            arguments = listOf(navArgument("data") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val dataJson = backStackEntry.arguments!!.getString("data") ?: ""
-            val gameCreated = Json.decodeFromString<GameCreated>(dataJson)
+        composable<Routes.Game> { backStackEntry ->
+            val game = remember { requireNotNull(backStackEntry.toRoute<Routes.Game>()) }
+            val gameCreated = remember(game) { Json.decodeFromString<GameCreated>(game.encodedGameCreated) }
             GameScreen(
                 gameViewModel = koinViewModel(parameters = { parametersOf(gameCreated) })
             )
         }
     }
+}
+
+
+sealed class Routes {
+    @Serializable
+    data object Login: Routes()
+
+    @Serializable
+    data class Game(val encodedGameCreated: String): Routes()
 }
