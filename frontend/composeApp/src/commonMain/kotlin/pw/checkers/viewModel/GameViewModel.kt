2ebeaa6b24b4d24c6f20b1f88275994957045ccd
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-16 23:42:27 +0100

    add message handling to GameViewModel

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/viewModel/GameViewModel.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/viewModel/GameViewModel.kt
@@ -3,20 +3,40 @@ package pw.checkers.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
 import pw.checkers.client.RealtimeMessageClient
+import pw.checkers.data.Content
 import pw.checkers.data.domain.Move
 import pw.checkers.data.domain.Cell
 import pw.checkers.data.domain.PieceType
 import pw.checkers.models.createInitialBoard
 import pw.checkers.data.domain.PlayerColor
+import pw.checkers.data.message.Message
+import pw.checkers.data.messageType.MessageType
+import pw.checkers.data.request.GetPossibilities
+import pw.checkers.data.request.MakeMove
 import pw.checkers.data.response.GameCreated
+import pw.checkers.data.response.GameEnd
+import pw.checkers.data.response.MoveInfo
+import pw.checkers.data.response.Possibilities
+import pw.checkers.util.handleMessageContent
 import kotlin.math.abs
 
-class GameViewModel(gameCreated: GameCreated, messageClient: RealtimeMessageClient) : ViewModel() {
+class GameViewModel(gameCreated: GameCreated, private val messageClient: RealtimeMessageClient) : ViewModel() {
 
     private val color = gameCreated.color
     private val gameId = gameCreated.gameId
     private val opponent = gameCreated.opponent
+
+    init {
+        viewModelScope.launch {
+            messageClient.getMessageStream().collect { message ->
+                handleMessage(message)
+            }
+        }
+    }
 
     private val _board = MutableStateFlow(createInitialBoard())
     val board = _board.map { board ->
@@ -46,35 +66,34 @@ class GameViewModel(gameCreated: GameCreated, messageClient: RealtimeMessageClie
     }
 
     fun getPossibleMoves(row: Int, col: Int) {
-        // TODO: actual implementation instead of placeholder
         if (checkSkipClick(row, col)) {
             return
         }
 
-        val newCell = Cell((3..4).random(), (0..7).random())
-        val newHighlighted = listOf(newCell)
-
         selected = Cell(row, col)
+        sendMessage(MessageType.POSSIBILITIES, GetPossibilities(gameId, row, col))
 
-        _highlightedCells.value = newHighlighted
-        println("clicked ($row, $col) - possible move (${newCell.row}, ${newCell.col})")
-        println(newHighlighted)
+        println("clicked ($row, $col)")
     }
 
-    private fun setHighlighted(cells: List<Cell>) {}
-
     fun makeMove(row: Int, col: Int) {
-        // TODO: actual implementation instead of placeholder
         _highlightedCells.value = emptyList()
+        val move = Move(selected.row, selected.col, row, col)
+
+        sendMessage(MessageType.MOVE, MakeMove(gameId, move))
 
         var captured: Cell? = null
         if (abs(selected.row - row) > 1) {
             captured = Cell((selected.row + row) / 2, (selected.col + col) / 2)
         }
-        movePiece(Move(selected.row, selected.col, row, col), captured)
+        movePiece(move, captured)
 
         selected = Cell(-1, -1)
         println("Moved to ($row, $col)")
+    }
+
+    private fun setHighlighted(cells: List<Cell>) {
+        _highlightedCells.value = cells
     }
 
     private fun movePiece(move: Move, captured: Cell? = null) {
@@ -97,9 +116,6 @@ class GameViewModel(gameCreated: GameCreated, messageClient: RealtimeMessageClie
         newBoard[move.toRow] = newPieceRow.toList()
         newBoard[move.fromRow] = currentPieceRow.toList()
 
-        println(newBoard[move.toRow])
-        println(newBoard[move.fromRow])
-
         captured?.let {
             val capturedRow = newBoard[captured.row].toMutableList()
             capturedRow[captured.col] = captured
@@ -107,7 +123,6 @@ class GameViewModel(gameCreated: GameCreated, messageClient: RealtimeMessageClie
         }
 
         _board.value = newBoard.toList()
-        println(_board.value)
     }
 
     private fun checkSkipClick(row: Int, col: Int): Boolean {
@@ -120,6 +135,44 @@ class GameViewModel(gameCreated: GameCreated, messageClient: RealtimeMessageClie
             PlayerColor.BLACK to PieceType.PAWN -> pieceCell.row == 7
             PlayerColor.WHITE to PieceType.PAWN -> pieceCell.row == 0
             else -> false
+        }
+    }
+
+    private fun handleMessage(msg: Message) {
+        println(msg)
+        when (msg.type) {
+            MessageType.MOVE -> handleMessageContent<MoveInfo>(msg, ::processMoveInfoMessage)
+            MessageType.POSSIBILITIES -> handleMessageContent<Possibilities>(msg, ::processPossibilities)
+            MessageType.GAME_ENDING -> handleMessageContent<GameEnd>(msg, ::processGameEnd)
+            else -> return
+        }
+    }
+
+    private fun processMoveInfoMessage(moveInfo: MoveInfo) {
+        if (moveInfo.previousTurn != _currentPlayer.value) {
+            movePiece(moveInfo.move, moveInfo.capturedPiece)
+            _currentPlayer.value = moveInfo.currentTurn
+        }
+
+    }
+
+    private fun processPossibilities(possibilities: Possibilities) {
+        setHighlighted(possibilities.moves)
+    }
+
+    private fun processGameEnd(gameEnd: GameEnd) {
+        println(gameEnd)
+    }
+
+    private inline fun <reified T: Content> sendMessage(type: MessageType, content: T) {
+        val message = Message(
+            type = type,
+            content = Json.encodeToJsonElement<T>(content)
+        )
+
+        viewModelScope.launch {
+            println(message)
+            messageClient.sendMessage(message)
         }
     }
 }
