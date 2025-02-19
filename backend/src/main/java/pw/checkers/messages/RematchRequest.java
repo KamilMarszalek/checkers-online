1f59f8b85ddb50e0b644c3db0dc556df4667b003
KamilMarszalek <kamilmar553@gmail.com>
2025-02-19 14:09:24 +0100

    Added class for rematch request message

---
+++ backend/src/main/java/pw/checkers/messages/RematchRequest.java
@@ -0,0 +1,14 @@
+package pw.checkers.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RematchRequest {
+    private String gameId;
+}
