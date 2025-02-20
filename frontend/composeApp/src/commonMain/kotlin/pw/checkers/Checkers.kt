648e632c46177833cc23b4a69ee4f52a7ff5c306
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-20 22:03:40 +0100

    add new screen to navigation, rework passing custom data through screens

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/Checkers.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/Checkers.kt
@@ -1,52 +1,65 @@
 package pw.checkers
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
-import pw.checkers.data.response.GameCreated
+import pw.checkers.data.message.Message
+import pw.checkers.data.response.GameInfo
+import pw.checkers.navigation.serializableNavType
 import pw.checkers.ui.screens.GameScreen
 import pw.checkers.ui.screens.LoginScreen
+import pw.checkers.ui.screens.WaitingScreen
+import kotlin.reflect.typeOf
 
 @Composable
 fun Checkers(navController: NavHostController = rememberNavController()) {
 
     NavHost(
         navController = navController,
-        startDestination = Routes.Login,
-//        modifier = Modifier.fillMaxSize(),
+        startDestination = Routes.LoginScreen,
     ) {
-        composable<Routes.Login> {
+        composable<Routes.LoginScreen> {
             LoginScreen(
-                koinViewModel(),
-                onLoginClick = { gameCreated ->
-                    navController.navigate(Routes.Game(Json.encodeToString(gameCreated)))
+                loginViewModel = koinViewModel(),
+                onPlayClick = { message ->
+                    navController.navigate(route = Routes.WaitingScreen(message))
                 }
             )
         }
 
-        composable<Routes.Game> { backStackEntry ->
-            val game = remember { requireNotNull(backStackEntry.toRoute<Routes.Game>()) }
-            val gameCreated = remember(game) { Json.decodeFromString<GameCreated>(game.encodedGameCreated) }
+        composable<Routes.WaitingScreen>(
+            typeMap = mapOf(typeOf<Message>() to serializableNavType<Message>())
+        ) { backStackEntry ->
+            val message = backStackEntry.toRoute<Routes.WaitingScreen>().message
+            WaitingScreen(
+                waitingViewModel = koinViewModel { parametersOf(message) },
+                onGameCreated = { gameInfo -> navController.navigate(Routes.GameScreen(gameInfo)) })
+        }
+
+        composable<Routes.GameScreen>(
+            typeMap = mapOf(typeOf<GameInfo>() to serializableNavType<GameInfo>())
+        ) { backStackEntry ->
+            val gameInfo = backStackEntry.toRoute<Routes.GameScreen>().gameInfo
             GameScreen(
-                gameViewModel = koinViewModel(parameters = { parametersOf(gameCreated) })
-            )
+                gameViewModel = koinViewModel { parametersOf(gameInfo) })
         }
     }
 }
 
 
-sealed class Routes {
+sealed interface Routes {
     @Serializable
-    data object Login: Routes()
+    data object LoginScreen : Routes
 
     @Serializable
-    data class Game(val encodedGameCreated: String): Routes()
+    data class WaitingScreen(val message: Message) : Routes
+
+    @Serializable
+    data class GameScreen(val gameInfo: GameInfo) : Routes
 }
