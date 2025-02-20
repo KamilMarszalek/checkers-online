a6601df0ca8b7f5f9df35784ae6b9e5abc6e517a
KamilMarszalek <kamilmar553@gmail.com>
2025-02-20 21:51:37 +0100

    Added @EqualsAndHashCode to User class

--- backend/src/main/java/pw/checkers/message/User.java
+++ backend/src/main/java/pw/checkers/message/User.java
@@ -1,14 +1,12 @@
 package pw.checkers.message;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class User {
     private String username;
 }
