1115546272a429be82a9cd726b18e52b785a5b87
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-20 21:59:31 +0100

    remove unused class

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/data/message/Message.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/data/message/Message.kt
@@ -5,9 +5,6 @@ import kotlinx.serialization.json.JsonElement
 import pw.checkers.data.messageType.MessageType
 
 @Serializable
-open class Content
-
-@Serializable
 data class Message(
     val type: MessageType,
     val content: JsonElement,
