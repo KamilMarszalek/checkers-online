52627503de0088964d462575f1cdbe0905d1612c
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-21 23:10:26 +0100

    rework navigation to work with added back to menu and next game functionality

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/Checkers.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/Checkers.kt
@@ -5,10 +5,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
 import androidx.navigation.toRoute
 import kotlinx.serialization.Serializable
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
+import pw.checkers.data.domain.User
 import pw.checkers.data.message.Message
 import pw.checkers.data.response.GameInfo
 import pw.checkers.navigation.serializableNavType
@@ -26,28 +28,43 @@ fun Checkers(navController: NavHostController = rememberNavController()) {
     ) {
         composable<Routes.LoginScreen> {
             LoginScreen(
-                loginViewModel = koinViewModel(),
-                onPlayClick = { message ->
-                    navController.navigate(route = Routes.WaitingScreen(message))
-                }
-            )
+                loginViewModel = koinViewModel(), onPlayClick = { message, user ->
+                    navController.navigate(route = Routes.WaitingScreen(message, user))
+                })
         }
 
         composable<Routes.WaitingScreen>(
-            typeMap = mapOf(typeOf<Message>() to serializableNavType<Message>())
+            typeMap = mapOf(
+                typeOf<Message>() to serializableNavType<Message>(), typeOf<User>() to serializableNavType<User>()
+            )
         ) { backStackEntry ->
-            val message = backStackEntry.toRoute<Routes.WaitingScreen>().message
+            val route = backStackEntry.toRoute<Routes.WaitingScreen>()
             WaitingScreen(
-                waitingViewModel = koinViewModel { parametersOf(message) },
-                onGameCreated = { gameInfo -> navController.navigate(Routes.GameScreen(gameInfo)) })
+                waitingViewModel = koinViewModel { parametersOf(route.message, route.user) },
+                onGameCreated = { gameInfo, user ->
+                    navController.popAndNavigate(Routes.GameScreen(gameInfo, user))
+                })
         }
 
         composable<Routes.GameScreen>(
-            typeMap = mapOf(typeOf<GameInfo>() to serializableNavType<GameInfo>())
+            typeMap = mapOf(
+                typeOf<GameInfo>() to serializableNavType<GameInfo>(), typeOf<User>() to serializableNavType<User>()
+            )
         ) { backStackEntry ->
-            val gameInfo = backStackEntry.toRoute<Routes.GameScreen>().gameInfo
+            val route = backStackEntry.toRoute<Routes.GameScreen>()
             GameScreen(
-                gameViewModel = koinViewModel { parametersOf(gameInfo) })
+                gameViewModel = koinViewModel { parametersOf(route.gameInfo, route.user) },
+                onMainMenuClick = {
+                    navController.navigate(
+                        route = Routes.LoginScreen, navOptions = navOptions {
+                            popUpTo<Routes.LoginScreen> { saveState = false }
+                        })
+                },
+                nextGame = { message, user ->
+                    navController.popAndNavigate(Routes.WaitingScreen(message, user))
+                },
+                rematch = { println("Rematch") },
+            )
         }
     }
 }
