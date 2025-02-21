82ea68e010cbc3d54c16a567219f45795cb31fee
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-21 23:10:32 +0100

    remove comments

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/client/KtorRealtimeMessageClient.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/client/KtorRealtimeMessageClient.kt
@@ -42,18 +42,6 @@ class KtorRealtimeMessageClient(private val httpClient: HttpClient) : RealtimeMe
         }
     }
 
-//    override suspend fun getMessageStream(): Flow<Message> {
-//        return flow {
-//            val messages = session!!
-//                .incoming
-//                .consumeAsFlow()
-//                .filterIsInstance<Frame.Text>()
-//                .mapNotNull { Json.decodeFromString<Message>(it.readText()) }
-//
-//            emitAll(messages)
-//        }
-//    }
-
     override suspend fun sendMessage(message: Message) {
         session?.outgoing?.send(Frame.Text(Json.encodeToString(message)))
     }
