46a5181036992a2b522c22764ce19e6382ab25b3
KamilMarszalek <kamilmar553@gmail.com>
2025-02-25 22:50:13 +0100

    Modified WebSocketConfig

--- backend/src/main/java/pw/checkers/config/WebSocketConfig.java
+++ backend/src/main/java/pw/checkers/config/WebSocketConfig.java
@@ -4,19 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.*;
 import pw.checkers.game.GameService;
 import pw.checkers.sockets.CheckersWebSocketHandler;
+import pw.checkers.sockets.SessionManager;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     private final GameService gameService;
+    private final SessionManager sessionManager;
 
-    public WebSocketConfig(GameService gameService) {
+    public WebSocketConfig(GameService gameService, SessionManager sessionManager) {
         this.gameService = gameService;
+        this.sessionManager = sessionManager;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new CheckersWebSocketHandler(gameService), "/ws")
+        registry.addHandler(new CheckersWebSocketHandler(gameService, sessionManager), "/ws")
                 .setAllowedOrigins("*");
     }
 }
