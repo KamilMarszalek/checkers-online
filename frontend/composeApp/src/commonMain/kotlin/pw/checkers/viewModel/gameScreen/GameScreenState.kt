0cda5a91dbec3e63ddb59cb9ac1e4202ddbb77c7
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-02-25 14:12:48 +0100

    add new GameScreenStates

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/viewModel/gameScreen/GameScreenState.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/viewModel/gameScreen/GameScreenState.kt
@@ -5,8 +5,9 @@ import pw.checkers.data.message.Message
 import pw.checkers.viewModel.ScreenState
 
 sealed interface GameScreenState : ScreenState {
-    data object Game : GameScreenState
     data class GameEnded(val result: Result) : GameScreenState
     data class PlayNext(val message: Message) : GameScreenState
     data object RematchPending : GameScreenState
+    data object RematchRequested : GameScreenState
+    data object RematchRejected : GameScreenState
 }
