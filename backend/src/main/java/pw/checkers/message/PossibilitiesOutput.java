d241be7767da0d4783d2afea9b1cefb290963a6b
KamilMarszalek <kamilmar553@gmail.com>
2025-03-03 22:51:01 +0100

    Renamed PossibleMoves to PossibilitiesOutput

--- backend/src/main/java/pw/checkers/message/PossibilitiesOutput.java
+++ backend/src/main/java/pw/checkers/message/PossibilitiesOutput.java
@@ -3,6 +3,7 @@ package pw.checkers.message;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import pw.checkers.data.enums.MessageType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,10 +11,11 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
-public class PossibleMoves {
+public class PossibilitiesOutput extends Message{
     private List<MoveHelper> moves;
 
-    public PossibleMoves() {
+    public PossibilitiesOutput() {
+        super(MessageType.POSSIBILITIES.getValue());
         this.moves = new ArrayList<>();
     }
 }
