f429c16bb2ec97a52172e4006c9d3d7b418390a7
KamilMarszalek <kamilmar553@gmail.com>
2025-03-03 23:08:20 +0100

    Deleted unused constructor

--- backend/src/main/java/pw/checkers/message/PossibilitiesInput.java
+++ backend/src/main/java/pw/checkers/message/PossibilitiesInput.java
@@ -1,19 +1,18 @@
 package pw.checkers.message;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pw.checkers.data.enums.MessageType;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PossibilitiesInput extends Message{
     private String gameId;
     private int row;
     private int col;
 
-    public PossibilitiesInput() {
-        super(MessageType.POSSIBILITIES.getValue());
-    }
     public PossibilitiesInput(String gameId, int row, int col) {
         super(MessageType.POSSIBILITIES.getValue());
         this.gameId = gameId;
