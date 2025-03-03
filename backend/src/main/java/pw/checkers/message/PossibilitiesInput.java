14b1397c01422ea8af62c7395295419e45e5e2a0
KamilMarszalek <kamilmar553@gmail.com>
2025-03-03 22:55:19 +0100

    Made PossibilitiesInput extend Message

--- backend/src/main/java/pw/checkers/message/PossibilitiesInput.java
+++ backend/src/main/java/pw/checkers/message/PossibilitiesInput.java
@@ -1,16 +1,23 @@
 package pw.checkers.message;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pw.checkers.data.enums.MessageType;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class PossibilitiesInput {
+public class PossibilitiesInput extends Message{
     private String gameId;
     private int row;
     private int col;
+
+    public PossibilitiesInput() {
+        super(MessageType.POSSIBILITIES.getValue());
+    }
+    public PossibilitiesInput(String gameId, int row, int col) {
+        super(MessageType.POSSIBILITIES.getValue());
+        this.gameId = gameId;
+        this.row = row;
+        this.col = col;
+    }
 }
