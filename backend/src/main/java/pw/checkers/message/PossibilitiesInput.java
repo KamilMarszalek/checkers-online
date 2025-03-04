fe2e8c9c2a7e5dbe03b91d753623748519437883
KamilMarszalek <kamilmar553@gmail.com>
2025-03-04 19:46:27 +0100

    Made PossibilitiesInput implement MessageAccept interface

--- backend/src/main/java/pw/checkers/message/PossibilitiesInput.java
+++ backend/src/main/java/pw/checkers/message/PossibilitiesInput.java
@@ -3,12 +3,14 @@ package pw.checkers.message;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.socket.WebSocketSession;
 import pw.checkers.data.enums.MessageType;
+import pw.checkers.sockets.MessageVisitor;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class PossibilitiesInput extends Message{
+public class PossibilitiesInput extends Message implements MessageAccept{
     private String gameId;
     private int row;
     private int col;
@@ -18,5 +20,10 @@ public class PossibilitiesInput extends Message{
         this.gameId = gameId;
         this.row = row;
         this.col = col;
+    }
+
+    @Override
+    public void accept(MessageVisitor visitor, WebSocketSession session) throws Exception {
+        visitor.visit(this, session);
     }
 }
