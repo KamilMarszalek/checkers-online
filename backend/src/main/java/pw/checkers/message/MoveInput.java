abbcce452f38413d2f1111cba695a9f6779e5a05
KamilMarszalek <kamilmar553@gmail.com>
2025-03-04 19:45:56 +0100

    Made MoveInput implement MessageAccept interface

--- backend/src/main/java/pw/checkers/message/MoveInput.java
+++ backend/src/main/java/pw/checkers/message/MoveInput.java
@@ -1,13 +1,20 @@
 package pw.checkers.message;
 
 import lombok.*;
+import org.springframework.web.socket.WebSocketSession;
+import pw.checkers.sockets.MessageVisitor;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
-public class MoveInput extends Message {
+public class MoveInput extends Message implements MessageAccept{
     private String gameId;
     private Move move;
+
+    @Override
+    public void accept(MessageVisitor visitor, WebSocketSession session) throws Exception {
+        visitor.visit(this, session);
+    }
 }
