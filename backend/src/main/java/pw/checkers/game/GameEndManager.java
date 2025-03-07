594d6add9c153f9b40fd250720db58277f495532
Kamil Marszałek <kamilmar553@gmail.com>
2025-03-07 21:59:52 +0100

    Implemented setGameEndReason

--- backend/src/main/java/pw/checkers/game/GameEndManager.java
+++ backend/src/main/java/pw/checkers/game/GameEndManager.java
@@ -3,6 +3,7 @@ package pw.checkers.game;
 import org.springframework.stereotype.Service;
 import pw.checkers.data.GameState;
 import pw.checkers.data.enums.Color;
+import pw.checkers.data.enums.GameEndReason;
 
 @Service
 public class GameEndManager {
@@ -41,6 +42,18 @@ public class GameEndManager {
     }
 
     public void setGameEndReason(GameState gameState, boolean resigned) {
-
+        if (resigned) {
+            gameState.setGameEndReason(GameEndReason.RESIGNATION);
+            return;
+        }
+        if (gameState.getWhitePiecesLeft() == 0 || gameState.getBlackPiecesLeft() == 0) {
+            gameState.setGameEndReason(GameEndReason.NO_PIECES);
+        } else if (!gameRules.playerHasMoves(gameState, Color.BLACK) || !gameRules.playerHasMoves(gameState, Color.WHITE)) {
+            gameState.setGameEndReason(GameEndReason.NO_MOVES);
+        } else if (gameState.getNoCapturesCounter() >= 50) {
+            gameState.setGameEndReason(GameEndReason.FIFTY_MOVES);
+        } else if (gameState.getNumberOfPositions().containsValue(3)) {
+            gameState.setGameEndReason(GameEndReason.THREEFOLD_REPETITION);
+        }
     }
 }
