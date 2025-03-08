1f76e7b82baa8cfcaa5e476453b457415d7a5ca8
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-03-08 16:43:17 +0100

    fix UserPanel with AutoSizeText

--- frontend/composeApp/src/commonMain/kotlin/pw/checkers/game/presentation/gameScreen/ui/components/UserPanel.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/checkers/game/presentation/gameScreen/ui/components/UserPanel.kt
@@ -4,29 +4,35 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.dshatz.composempp.AutoSizeText
 import pw.checkers.game.domain.model.User
 
 @Composable
-fun UserPanel(modifier: Modifier, user: User, isCurrentTurn: Boolean) {
+fun UserPanel(
+    user: User,
+    isCurrentTurn: Boolean,
+    modifier: Modifier = Modifier,
+) {
     val backgroundColor = if (isCurrentTurn) MaterialTheme.colorScheme.secondaryContainer
-        else MaterialTheme.colorScheme.background
+    else MaterialTheme.colorScheme.background
 
     val textColor = if (isCurrentTurn) MaterialTheme.colorScheme.onSecondaryContainer
-        else MaterialTheme.colorScheme.onBackground
+    else MaterialTheme.colorScheme.onBackground
 
     Box(modifier = modifier.background(color = backgroundColor)) {
-        Text(
+        AutoSizeText(
             text = user.username,
-            modifier = Modifier.align(Alignment.CenterStart).padding(8.dp),
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(8.dp),
             fontWeight = if (isCurrentTurn) FontWeight.Bold else FontWeight.Normal,
-            fontSize = MaterialTheme.typography.headlineMedium.fontSize,
             color = textColor,
+            style = MaterialTheme.typography.headlineMedium,
         )
     }
 }
