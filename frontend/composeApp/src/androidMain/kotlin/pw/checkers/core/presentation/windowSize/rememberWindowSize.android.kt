313ae220d5e1e9a46c3d4693bc49bf337aa747fd
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-03-08 16:40:12 +0100

    remove rememberWindowSize

--- frontend/composeApp/src/androidMain/kotlin/pw/checkers/core/presentation/windowSize/rememberWindowSize.android.kt
+++
@@ -1,8 +0,0 @@
-package pw.checkers.core.presentation.windowSize
-
-import androidx.compose.runtime.Composable
-
-@Composable
-actual fun rememberWindowSize(): WindowSize {
-    TODO("Not yet implemented")
-}
