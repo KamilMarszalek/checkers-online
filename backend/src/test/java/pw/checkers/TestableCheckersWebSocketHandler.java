6e865d714cae3168f25145d529f3aae580cbfe98
Kamil Marszałek <kamilmar553@gmail.com>
2025-03-09 19:04:31 +0100

    Fixed TestableCheckersWebSocketHandler

--- backend/src/test/java/pw/checkers/TestableCheckersWebSocketHandler.java
+++ backend/src/test/java/pw/checkers/TestableCheckersWebSocketHandler.java
@@ -3,11 +3,21 @@ package pw.checkers;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import pw.checkers.sockets.*;
+import pw.checkers.sockets.handlers.*;
 
 public class TestableCheckersWebSocketHandler extends CheckersWebSocketHandler {
-    public TestableCheckersWebSocketHandler(SessionManager sessionManager, RematchService rematchService,
-                                            MessageSender messageSender, GameManager gameManager) {
-        super(sessionManager, rematchService, messageSender, gameManager);
+    public TestableCheckersWebSocketHandler(SessionManager sessionManager, JoinQueueHandler joinQueueHandler, LeaveQueueHandler leaveQueueHandler, MoveHandler moveHandler, PossibilitiesHandler possibilitiesHandler, AcceptRematchHandler acceptRematchHandler, LeaveHandler leaveHandler, DeclineRematchHandler declineRematchHandler, RematchRequestHandler rematchRequestHandler, ResignHandler resignHandler, MessageMapper messageMapper) {
+        super(sessionManager,
+                joinQueueHandler,
+                leaveQueueHandler,
+                moveHandler,
+                possibilitiesHandler,
+                acceptRematchHandler,
+                leaveHandler,
+                declineRematchHandler,
+                rematchRequestHandler,
+                resignHandler,
+                messageMapper);
     }
 
     @Override
